df3408a69cd43ffaf3e0e07b8c847b14e9dc1630
vwxyzh <vwxyzh@gmail.com>
2016-07-28 23:13:00 +0800

    add files.

---
+++ demo/HtmlTagValidation/RequireClassAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Composition;
+using System.Text.RegularExpressions;
+
+using Microsoft.DocAsCode.Plugins;
+
+namespace HtmlTagValidation
+{
+    [Export("require_class_attibute", typeof(ICustomMarkdownTagValidator))]
+    public class RequireClassAttribute : ICustomMarkdownTagValidator
+    {
+        public bool Validate(string tag)
+        {
+            return Regex.IsMatch(tag, "\\bclass\\s+=\\s+\\\"");
+        }
+    }
+}
---
+++ demo/MarkdownValidation/HeadingLimitation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+
+using Microsoft.DocAsCode.Common;
+using Microsoft.DocAsCode.MarkdownLite;
+
+namespace MarkdownValidation
+{
+    [Export("heading_limitation", typeof(IMarkdownTokenValidatorProvider))]
+    public class HeadingLimitation : IMarkdownTokenValidatorProvider
+    {
+        public ImmutableArray<IMarkdownTokenValidator> GetValidators()
+        {
+            return ImmutableArray.Create(
+                MarkdownTokenValidatorFactory.FromLambda<MarkdownHeadingBlockToken>(
+                    token =>
+                    {
+                        if (token.Depth > 4)
+                        {
+                            Logger.LogWarning("Invalid heading", line: token.SourceInfo.LineNumber.ToString());
+                        }
+                    }));
+        }
+    }
+}
