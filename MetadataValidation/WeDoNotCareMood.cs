fa816a43bf0400d50ed19b4d7845bdb0d6c808a8
vwxyzh <vwxyzh@gmail.com>
2016-07-29 00:04:29 +0800

    add metadata validations.

---
+++ MetadataValidation/WeDoNotCareMood.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+
+using Microsoft.DocAsCode.Common;
+using Microsoft.DocAsCode.Plugins;
+
+namespace MetadataValidation
+{
+    [Export(typeof(IInputMetadataValidator))]
+    public class WeDoNotCareMood : IInputMetadataValidator
+    {
+        public void Validate(string sourceFile, ImmutableDictionary<string, object> metadata)
+        {
+            if (metadata.ContainsKey("mood"))
+            {
+                Logger.LogWarning("We don't care mood!", file: sourceFile);
+            }
+        }
+    }
+}
